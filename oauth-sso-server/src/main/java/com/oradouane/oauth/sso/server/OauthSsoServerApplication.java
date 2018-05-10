3d010eb256b255ab1d0e6efc3ae6be70851202fc
ouledmoussa <radouane.ouledmoussa@ilemgroup.com>
2018-05-10 22:14:28 +0100

    code clean

--- oauth-sso-server/src/main/java/com/oradouane/oauth/sso/server/OauthSsoServerApplication.java
+++ oauth-sso-server/src/main/java/com/oradouane/oauth/sso/server/OauthSsoServerApplication.java
@@ -2,13 +2,11 @@ package com.oradouane.oauth.sso.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-//@EnableResourceServer
 public class OauthSsoServerApplication {
 
     public static void main(String[] args) {
